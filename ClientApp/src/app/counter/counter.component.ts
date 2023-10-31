fa9b938089b6197259fcdda18407cf177c5f772c
Mackay Fisher <mackayfisher@Mackays-MacBook-Air.local>
2023-10-31 13:56:44 -0500

    added the contoller realation for TodoItems but behinf on naming as well as fucntion generation

--- ClientApp/src/app/counter/counter.component.ts
+++ ClientApp/src/app/counter/counter.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class CounterComponent {
   public currentCount = 0;
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+  }
 
-  public incrementCounter() {
-    this.currentCount++;
+  getAllTasks(){
+    
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using AngularApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
